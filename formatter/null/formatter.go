cd72ab556d0580115ef036b720f93a8dcdd1207e
gratonos <gratonos@outlook.com>
2018-09-15 11:28:37 +0800

    feat(formatter/null): return by function instead of be public

--- formatter/null/formatter.go
+++ formatter/null/formatter.go
@@ -5,4 +5,8 @@ import (
 	"github.com/gratonos/gxlog/formatter"
 )
 
-var Formatter = formatter.FormatterFunc(func(*gxlog.Record) []byte { return nil })
+var nullFormatter = formatter.FormatterFunc(func(*gxlog.Record) []byte { return nil })
+
+func Formatter() gxlog.Formatter {
+	return nullFormatter
+}
