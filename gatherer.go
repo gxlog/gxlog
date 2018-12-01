76f2a1ad4655e4937518b99ada1d201855260e33
gratonos <gratonos@outlook.com>
2018-12-01 11:49:57 +0800

    perf(gxlog): reuse value of Record

--- gatherer.go
+++ gatherer.go
@@ -5,10 +5,15 @@ import (
 	"time"
 )
 
-type gatherer struct{}
+type gatherer struct {
+	record Record
+}
 
 func (this *gatherer) Gather(calldepth int, level LogLevel, msg string) *Record {
-	now := time.Now()
+	this.record.Time = time.Now()
+	this.record.Level = level
+	this.record.Msg = msg
+
 	var funcName string
 	pc, file, line, ok := runtime.Caller(calldepth)
 	if !ok {
@@ -18,12 +23,12 @@ func (this *gatherer) Gather(calldepth int, level LogLevel, msg string) *Record 
 	} else {
 		funcName = runtime.FuncForPC(pc).Name()
 	}
-	return &Record{
-		Time:     now,
-		Level:    level,
-		Pathname: file,
-		Line:     line,
-		Func:     funcName,
-		Msg:      msg,
-	}
+	this.record.Pathname = file
+	this.record.Line = line
+	this.record.Func = funcName
+
+	this.record.Prefix = ""
+	this.record.Contexts = this.record.Contexts[:0]
+
+	return &this.record
 }
