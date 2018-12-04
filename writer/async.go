8c51468aee0bf7319f1226ce68949c2caa9e2b56
gratonos <gratonos@outlook.com>
2018-12-05 03:43:04 +0800

    feat(writer): add support for asynchronous

---
+++ writer/async.go
@@ -0,0 +1,54 @@
+package writer
+
+import (
+	"github.com/gratonos/gxlog"
+)
+
+type logData struct {
+	bs     []byte
+	record *gxlog.Record
+}
+
+type Async struct {
+	writer    gxlog.Writer
+	chanData  chan logData
+	chanClose chan struct{}
+}
+
+func NewAsync(wt gxlog.Writer, chanLen int) *Async {
+	async := &Async{
+		writer:    wt,
+		chanData:  make(chan logData, chanLen),
+		chanClose: make(chan struct{}),
+	}
+	go async.serve()
+	return async
+}
+
+func (this *Async) Write(bs []byte, record *gxlog.Record) {
+	this.chanData <- logData{bs: bs, record: record}
+}
+
+func (this *Async) Close() {
+	close(this.chanClose)
+	close(this.chanData)
+	for data := range this.chanData {
+		this.writer.Write(data.bs, data.record)
+	}
+}
+
+func (this *Async) Abort() {
+	close(this.chanClose)
+	close(this.chanData)
+}
+
+func (this *Async) serve() {
+	for {
+		select {
+		case data := <-this.chanData:
+			this.writer.Write(data.bs, data.record)
+		case <-this.chanClose:
+			break
+		}
+	}
+}
