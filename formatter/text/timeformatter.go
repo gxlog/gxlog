93d171a8505feb908faafe6d08c1f801aa54f602
gratonos <gratonos@outlook.com>
2018-12-06 21:17:17 +0800

    feat(formatter/text): add predefined layout for time formatter

--- formatter/text/timeformatter.go
+++ formatter/text/timeformatter.go
@@ -2,32 +2,78 @@ package text
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gratonos/gxlog"
 )
 
 const DefaultTimeLayout = "2006-01-02 15:04:05.000000"
 
+const (
+	cFmtDate  = "2006-01-02"
+	cFmtTime  = "15:04:05"
+	cFmtMilli = ".000"
+	cFmtMicro = ".000000"
+	cFmtNano  = ".000000000"
+)
+
 type timeFormatter struct {
-	property string
-	fmtspec  string
+	layout  string
+	fmtspec string
 }
 
 func newTimeFormatter(property, fmtspec string) *timeFormatter {
-	if property == "" {
-		property = DefaultTimeLayout
-	}
 	if fmtspec == "" {
 		fmtspec = "%s"
 	}
-	return &timeFormatter{property: property, fmtspec: fmtspec}
+	return &timeFormatter{
+		layout:  makeTimeLayout(property),
+		fmtspec: fmtspec,
+	}
 }
 
 func (this *timeFormatter) FormatElement(buf []byte, record *gxlog.Record) []byte {
-	desc := record.Time.Format(this.property)
+	desc := record.Time.Format(this.layout)
 	if this.fmtspec == "%s" {
 		return append(buf, desc...)
 	} else {
 		return append(buf, fmt.Sprintf(this.fmtspec, desc)...)
 	}
+}
+
+func makeTimeLayout(property string) string {
+	if strings.ContainsAny(property, "0123456789") {
+		return property
+	}
+
+	var layout string
+	timeType, decimalType := getTimeOptions(property)
+	switch timeType {
+	case "date":
+		layout = cFmtDate + " " + cFmtTime
+	case "time":
+		layout = cFmtTime
+	default:
+		return DefaultTimeLayout
+	}
+	switch decimalType {
+	case "ms":
+		layout += cFmtMilli
+	case "us":
+		layout += cFmtMicro
+	case "ns":
+		layout += cFmtNano
+	}
+	return layout
+}
+
+func getTimeOptions(str string) (string, string) {
+	fields := strings.Split(strings.ToLower(str), ".")
+	if len(fields) == 0 {
+		return "", ""
+	}
+	if len(fields) == 1 {
+		return fields[0], ""
+	}
+	return fields[0], fields[1]
 }
