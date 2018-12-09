623462ab74f332c4070c44bb114f4138d14e8443
gratonos <gratonos@outlook.com>
2018-12-09 17:59:30 +0800

    feat(gxlog,defaults): update link related methods of logger

--- defaults/logger.go
+++ defaults/logger.go
@@ -14,7 +14,7 @@ var gFormatter *text.Formatter
 func init() {
 	gLogger = gxlog.New(gxlog.NewConfig())
 	gFormatter = text.New(text.NewConfig())
-	gLogger.MustLink(gxlog.Slot0, gFormatter, writer.Wrap(os.Stderr))
+	gLogger.Link(gxlog.Slot0, gFormatter, writer.Wrap(os.Stderr))
 }
 
 func Logger() *gxlog.Logger {
--- link.go
+++ link.go
@@ -21,219 +21,11 @@ type link struct {
 	filter    Filter
 }
 
-func (this *logger) Link(slot Slot, ft Formatter, wt Writer, opts ...interface{}) bool {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	if this.slots[slot] != nil {
-		return false
-	}
-	this.link(slot, ft, wt, opts)
-	return true
-}
-
-func (this *logger) ForceLink(slot Slot, ft Formatter, wt Writer, opts ...interface{}) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	this.link(slot, ft, wt, opts)
-}
-
-func (this *logger) MustLink(slot Slot, ft Formatter, wt Writer, opts ...interface{}) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	if this.slots[slot] != nil {
-		panic("slot in use")
-	}
-	this.link(slot, ft, wt, opts)
-}
-
-func (this *logger) Unlink(slot Slot) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	this.slots[slot] = nil
-}
-
-func (this *logger) UnlinkAll() {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	for i := range this.slots {
-		this.slots[i] = nil
-	}
-}
-
-func (this *logger) CopyLink(dst, src Slot) bool {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	if this.slots[dst] != nil {
-		return false
-	}
-	this.slots[dst] = this.slots[src]
-	return true
-}
-
-func (this *logger) ForceCopyLink(dst, src Slot) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	this.slots[dst] = this.slots[src]
-}
-
-func (this *logger) MustCopyLink(dst, src Slot) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	if this.slots[dst] != nil {
-		panic("slot in use")
-	}
-	this.slots[dst] = this.slots[src]
-}
-
-func (this *logger) MoveLink(to, from Slot) bool {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	if this.slots[to] != nil {
-		return false
-	}
-	this.slots[to] = this.slots[from]
-	this.slots[from] = nil
-	return true
-}
-
-func (this *logger) ForceMoveLink(to, from Slot) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	this.slots[to] = this.slots[from]
-	this.slots[from] = nil
-}
-
-func (this *logger) MustMoveLink(to, from Slot) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	if this.slots[to] != nil {
-		panic("slot in use")
-	}
-	this.slots[to] = this.slots[from]
-	this.slots[from] = nil
-}
-
-func (this *logger) SwapLink(left, right Slot) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	this.slots[left], this.slots[right] = this.slots[right], this.slots[left]
-}
-
-func (this *logger) HasLink(slot Slot) bool {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	return this.slots[slot] != nil
-}
-
-func (this *logger) LinkFormatter(slot Slot) Formatter {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	link := this.slots[slot]
-	if link == nil {
-		return nil
-	}
-	return link.formatter
-}
-
-func (this *logger) SetLinkFormatter(slot Slot, ft Formatter) {
-	if ft == nil {
-		panic("nil formatter")
-	}
-
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	link := this.slots[slot]
-	if link != nil {
-		link.formatter = ft
-	}
-}
-
-func (this *logger) LinkWriter(slot Slot) Writer {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	link := this.slots[slot]
-	if link == nil {
-		return nil
-	}
-	return link.writer
-}
-
-func (this *logger) SetLinkWriter(slot Slot, wt Writer) {
-	if wt == nil {
-		panic("nil writer")
-	}
-
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	link := this.slots[slot]
-	if link != nil {
-		link.writer = wt
-	}
-}
-
-func (this *logger) LinkLevel(slot Slot) Level {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	link := this.slots[slot]
-	if link == nil {
-		return LevelOff
-	}
-	return link.level
-}
-
-func (this *logger) SetLinkLevel(slot Slot, level Level) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	link := this.slots[slot]
-	if link != nil {
-		link.level = level
-	}
-}
-
-func (this *logger) LinkFilter(slot Slot) Filter {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	link := this.slots[slot]
-	if link == nil {
-		return nil
-	}
-	return link.filter
-}
-
-func (this *logger) SetLinkFilter(slot Slot, filter Filter) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-
-	link := this.slots[slot]
-	if link != nil {
-		link.filter = filter
-	}
-}
-
-func (this *logger) link(slot Slot, ft Formatter, wt Writer, opts []interface{}) {
+func (this *logger) Link(slot Slot, ft Formatter, wt Writer, opts ...interface{}) {
 	if ft == nil || wt == nil {
 		panic("nil formatter or nil writer")
 	}
+
 	link := &link{
 		formatter: ft,
 		writer:    wt,
@@ -253,5 +45,180 @@ func (this *logger) link(slot Slot, ft Formatter, wt Writer, opts []interface{})
 			panic("unknown link option type")
 		}
 	}
+
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
 	this.slots[slot] = link
+}
+
+func (this *logger) Unlink(slot Slot) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	this.slots[slot] = nil
+}
+
+func (this *logger) UnlinkAll() {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	for i := range this.slots {
+		this.slots[i] = nil
+	}
+}
+
+func (this *logger) CopyLink(dst, src Slot) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	this.slots[dst] = this.slots[src]
+}
+
+func (this *logger) MoveLink(to, from Slot) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	this.slots[to] = this.slots[from]
+	this.slots[from] = nil
+}
+
+func (this *logger) SwapLink(left, right Slot) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	this.slots[left], this.slots[right] = this.slots[right], this.slots[left]
+}
+
+func (this *logger) Busy(slot Slot) bool {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	return this.slots[slot] != nil
+}
+
+func (this *logger) BusySlots() []Slot {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	var slots []Slot
+	for slot, link := range this.slots {
+		if link != nil {
+			slots = append(slots, Slot(slot))
+		}
+	}
+	return slots
+}
+
+func (this *logger) FreeSlots() []Slot {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	var slots []Slot
+	for slot, link := range this.slots {
+		if link == nil {
+			slots = append(slots, Slot(slot))
+		}
+	}
+	return slots
+}
+
+func (this *logger) LinkFormatter(slot Slot) Formatter {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	link := this.slots[slot]
+	if link == nil {
+		return nil
+	}
+	return link.formatter
+}
+
+func (this *logger) SetLinkFormatter(slot Slot, ft Formatter) bool {
+	if ft == nil {
+		panic("nil formatter")
+	}
+
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	link := this.slots[slot]
+	if link != nil {
+		link.formatter = ft
+		return true
+	}
+	return false
+}
+
+func (this *logger) LinkWriter(slot Slot) Writer {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	link := this.slots[slot]
+	if link == nil {
+		return nil
+	}
+	return link.writer
+}
+
+func (this *logger) SetLinkWriter(slot Slot, wt Writer) bool {
+	if wt == nil {
+		panic("nil writer")
+	}
+
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	link := this.slots[slot]
+	if link != nil {
+		link.writer = wt
+		return true
+	}
+	return false
+}
+
+func (this *logger) LinkLevel(slot Slot) Level {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	link := this.slots[slot]
+	if link == nil {
+		return LevelOff
+	}
+	return link.level
+}
+
+func (this *logger) SetLinkLevel(slot Slot, level Level) bool {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	link := this.slots[slot]
+	if link != nil {
+		link.level = level
+		return true
+	}
+	return false
+}
+
+func (this *logger) LinkFilter(slot Slot) Filter {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	link := this.slots[slot]
+	if link == nil {
+		return nil
+	}
+	return link.filter
+}
+
+func (this *logger) SetLinkFilter(slot Slot, filter Filter) bool {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	link := this.slots[slot]
+	if link != nil {
+		link.filter = filter
+		return true
+	}
+	return false
 }
