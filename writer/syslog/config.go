13ece80183046056c815c8e07db9b64894ca1baa
gratonos <gratonos@outlook.com>
2018-12-10 21:25:38 +0800

    feat(writer/syslog): init

---
+++ writer/syslog/config.go
@@ -0,0 +1,56 @@
+package syslog
+
+import (
+	"log/syslog"
+	"os"
+	"path/filepath"
+
+	"github.com/gratonos/gxlog"
+)
+
+const (
+	DefaultFacility    = syslog.LOG_USER
+	DefaultSeverity    = syslog.LOG_DEBUG
+	DefaultPriority    = DefaultFacility | DefaultSeverity
+	DefaultReportOnErr = true
+)
+
+type Config struct {
+	Tag         string
+	Priority    syslog.Priority
+	Network     string
+	Addr        string
+	SeverityMap map[gxlog.Level]syslog.Priority
+	ReportOnErr bool
+}
+
+func NewConfig(tag string) *Config {
+	if tag == "" {
+		tag = filepath.Base(os.Args[0])
+	}
+	return &Config{
+		Tag:         tag,
+		Priority:    DefaultPriority,
+		ReportOnErr: DefaultReportOnErr,
+	}
+}
+
+func (this *Config) WithPriority(priority syslog.Priority) *Config {
+	this.Priority = priority
+	return this
+}
+
+func (this *Config) WithAddr(network, addr string) *Config {
+	this.Network, this.Addr = network, addr
+	return this
+}
+
+func (this *Config) WithSeverityMap(severityMap map[gxlog.Level]syslog.Priority) *Config {
+	this.SeverityMap = severityMap
+	return this
+}
+
+func (this *Config) WithReportOnErr(ok bool) *Config {
+	this.ReportOnErr = ok
+	return this
+}
---
+++ writer/syslog/writer.go
@@ -0,0 +1,109 @@
+package syslog
+
+import (
+	"fmt"
+	"log"
+	"log/syslog"
+	"sync"
+
+	"github.com/gratonos/gxlog"
+)
+
+const (
+	DefaultTraceSeverity = syslog.LOG_DEBUG
+	DefaultDebugSeverity = syslog.LOG_DEBUG
+	DefaultInfoSeverity  = syslog.LOG_INFO
+	DefaultWarnSeverity  = syslog.LOG_WARNING
+	DefaultErrorSeverity = syslog.LOG_ERR
+	DefaultFatalSeverity = syslog.LOG_CRIT
+)
+
+const cSeverityMask = 0x07
+
+type syslogFunc func(string) error
+
+type Writer struct {
+	reportOnErr bool
+
+	logFuncs [gxlog.LevelCount]syslogFunc
+	writer   *syslog.Writer
+
+	lock sync.Mutex
+}
+
+func Open(config *Config) (*Writer, error) {
+	if config == nil {
+		panic("nil config")
+	}
+	wt, err := syslog.Dial(config.Network, config.Addr, config.Priority, config.Tag)
+	if err != nil {
+		return nil, fmt.Errorf("syslog.Open: %v", err)
+	}
+	writer := &Writer{
+		reportOnErr: config.ReportOnErr,
+		writer:      wt,
+	}
+	severityMap := map[gxlog.Level]syslog.Priority{
+		gxlog.LevelTrace: DefaultTraceSeverity,
+		gxlog.LevelDebug: DefaultDebugSeverity,
+		gxlog.LevelInfo:  DefaultInfoSeverity,
+		gxlog.LevelWarn:  DefaultWarnSeverity,
+		gxlog.LevelError: DefaultErrorSeverity,
+		gxlog.LevelFatal: DefaultFatalSeverity,
+	}
+	writer.updateLogFuncs(severityMap)
+	writer.updateLogFuncs(config.SeverityMap)
+	return writer, nil
+}
+
+func (this *Writer) Close() error {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	if err := this.writer.Close(); err != nil {
+		return fmt.Errorf("syslog.Close: %v", err)
+	}
+	return nil
+}
+
+func (this *Writer) Write(bs []byte, record *gxlog.Record) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	err := this.logFuncs[record.Level](string(bs))
+	if this.reportOnErr && err != nil {
+		log.Println("syslog.Write:", err)
+	}
+}
+
+func (this *Writer) MapSeverity(severityMap map[gxlog.Level]syslog.Priority) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	this.updateLogFuncs(severityMap)
+}
+
+func (this *Writer) updateLogFuncs(severityMap map[gxlog.Level]syslog.Priority) {
+	for level, severity := range severityMap {
+		var fn syslogFunc
+		switch severity & cSeverityMask {
+		case syslog.LOG_DEBUG:
+			fn = this.writer.Debug
+		case syslog.LOG_INFO:
+			fn = this.writer.Info
+		case syslog.LOG_NOTICE:
+			fn = this.writer.Notice
+		case syslog.LOG_WARNING:
+			fn = this.writer.Warning
+		case syslog.LOG_ERR:
+			fn = this.writer.Err
+		case syslog.LOG_CRIT:
+			fn = this.writer.Crit
+		case syslog.LOG_ALERT:
+			fn = this.writer.Alert
+		case syslog.LOG_EMERG:
+			fn = this.writer.Emerg
+		}
+		this.logFuncs[level] = fn
+	}
+}
