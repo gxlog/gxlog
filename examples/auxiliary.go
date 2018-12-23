c1d91027f75df944af280d0dc04b3ef1fbf52acb
gratonos <gratonos@outlook.com>
2018-12-23 14:07:05 +0800

    docs: add basic and auxiliary examples

---
+++ examples/auxiliary.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/gxlog/gxlog"
+	"github.com/gxlog/gxlog/defaults"
+)
+
+var log = defaults.Logger()
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func main() {
+	// Only supported on systems that ANSI escape sequences are supported.
+	defaults.Formatter().EnableColor()
+
+	// logs with mark will be colorized with magenta by default
+	log.WithPrefix("**** ").WithMark(true).WithContext("k1", "v1", "k2", "v2").
+		Info("prefix, mark and contexts")
+	// the instance of log is left to be unchanged
+	log.Info("no prefix, mark or contexts")
+
+	// demonstrates the lexical scope
+	func() {
+		log := log.WithContext("k3", "v3")
+		log.Info("outer enter")
+		func() {
+			log := log.WithContext("k4", "v4")
+			log.Info("inner")
+		}()
+		log.Info("outer leave")
+	}()
+
+	// all the key-value pairs of dynamic contexts will be concatenated to the
+	//   end of static contexts
+	// dynamic contexts are very useful when you want to print some/all fields
+	//   of a struct value all the time.
+	// ATTENTION: you should be very careful to concurrent safety or dead locks
+	//   with dynamic contexts.
+	n := 0
+	fn := gxlog.Dynamic(func(interface{}) interface{} {
+		n++
+		return n
+	})
+	clog := log.WithContext("static", n, "dynamic", fn)
+	clog.Info("dynamic one")
+	clog.Info("dynamic two")
+
+	// THINK TWICE before you limit logs output by count or by time,
+	//   you may miss logs which you need.
+	// only 2 logs will be output per 3 logs
+	for i := 1; i <= 6; i++ {
+		log.WithCountLimit(3, 2).Infof("count limited: %d", i)
+	}
+	// the more efficient way
+	llog := log.WithCountLimit(3, 2)
+	for i := 7; i <= 12; i++ {
+		llog.Infof("efficient count limited: %d", i)
+	}
+	// NOTICE: The space complexity is O(n), while n is the 2nd argument of
+	//   WithTimeLimit. Try to specify reasonable duration and limit.
+	// at most 3 logs will be output during any interval of 1 second
+	for i := 1; i <= 10; i++ {
+		log.WithTimeLimit(time.Second, 3).Infof("time limited: %d", i)
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+	}
+}
---
+++ examples/basic.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gxlog/gxlog"
+	"github.com/gxlog/gxlog/defaults"
+)
+
+var log = defaults.Logger()
+
+func main() {
+	// Only supported on systems that ANSI escape sequences are supported.
+	defaults.Formatter().EnableColor()
+
+	log.Trace("test Trace")
+	log.Tracef("%s", "test Tracef")
+	log.Debug("test Debug")
+	log.Debugf("%s", "test Debugf")
+	log.Info("test Info")
+	log.Infof("%s", "test Infof")
+	log.Warn("test Warn")
+	log.Warnf("%s", "test Warnf")
+	log.Error("test Error")
+	log.Errorf("%s", "test Errorf")
+	// Fatal and Fatalf will output the stack of current goroutine by default.
+	log.Fatal("test Fatal")
+	log.Fatalf("%s", "test Fatalf")
+
+	// The default level of Panic or Panicf is fatal.
+	// It will always panic no matter at which level the logger is.
+	// log.Panic("test Panic")
+	// log.Panicf("%s", "test Panicf")
+
+	// Time and Timef will return a function. When the function is called,
+	//   it will output the log as well as the time cost.
+	// The default level of Time and Timef is trace.
+	done := log.Time("test Time")
+	time.Sleep(200 * time.Millisecond)
+	done()
+	// Notice the last empty pair of parentheses.
+	defer log.Timef("%s", "test Timef")()
+	time.Sleep(400 * time.Millisecond)
+
+	// The calldepth can be specified in Log and Logf. That is useful when
+	//   you want to customize your own log helper functions.
+	log.Log(0, gxlog.LevelInfo, "test Log")
+	log.Logf(-1, gxlog.LevelWarn, "%s", "test Logf")
+
+	test1()
+	test2()
+}
+
+func test1() error {
+	// LogError will output log and call errors.New to generate an error
+	return log.LogError(gxlog.LevelError, "an error")
+}
+
+func test2() error {
+	// LogErrorf will output log and call fmt.Errorf to generate an error
+	return log.LogErrorf(gxlog.LevelError, "%s", "another error")
+}
