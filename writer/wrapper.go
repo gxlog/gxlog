0ae369cb6f6b7a30cde37b51c038ba921d3e2605
gratonos <gratonos@outlook.com>
2019-01-15 19:20:11 +0800

    refactor(writer): reimplement wrapper

--- writer/wrapper.go
+++ writer/wrapper.go
@@ -6,15 +6,9 @@ import (
 	"github.com/gxlog/gxlog/iface"
 )
 
-type wrapper struct {
-	writer io.Writer
-}
-
 // Wrap wraps an io.Writer to iface.Writer. The writer must NOT be nil.
 func Wrap(writer io.Writer) iface.Writer {
-	return &wrapper{writer: writer}
-}
-
-func (wrap *wrapper) Write(bs []byte, _ *iface.Record) {
-	wrap.writer.Write(bs)
+	return Func(func(bs []byte, _ *iface.Record) {
+		writer.Write(bs)
+	})
 }
