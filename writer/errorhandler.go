14e5d1d2f2f7b541b53d8d59f4f313825708af90
gratonos <gratonos@outlook.com>
2019-01-19 17:45:36 +0800

    fix(writer): fix the stack offset of Report and ReportDetails

--- writer/errorhandler.go
+++ writer/errorhandler.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gxlog/gxlog/iface"
 )
 
+const callDepthOffset = 6
+
 // The ErrorHandler type is a function type used to handle errors.
 // Do NOT call any method of the Writer or the Logger within the function,
 // or it may deadlock.
@@ -14,10 +16,10 @@ type ErrorHandler func(bs []byte, record *iface.Record, err error)
 
 // Report calls log.Output with the err.
 func Report(_ []byte, _ *iface.Record, err error) {
-	log.Output(1, fmt.Sprintln("log write error:", err))
+	log.Output(callDepthOffset, fmt.Sprintln("log error:", err))
 }
 
 // ReportDetails calls log.Output with the err and the bs.
 func ReportDetails(bs []byte, _ *iface.Record, err error) {
-	log.Output(1, fmt.Sprintf("log write error: %s, log: %s\n", err, bs))
+	log.Output(callDepthOffset, fmt.Sprintf("log error: %s, log: %s", err, bs))
 }
